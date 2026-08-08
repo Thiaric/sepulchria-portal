6346e993465e3fc7d0522cc68460c2f46426dea0
Riccardo Catanese <thiaric@gmail.com>
2026-08-08 13:20:47 +0100

    Images from all sources

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.mos.cms.futurecdn.net",
-      },
-      {
-        protocol: "https",
-        hostname: "*.supabase.co", 
-      },
-      {
-        protocol: "https",
-        hostname: "thenobleartist.com",
+        hostname: "**",
       },
     ],
   },
