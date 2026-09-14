f43f54d6239425c19601ec28b656ba9ed4c2d941
Riccardo Catanese <thiaric@gmail.com>
2026-09-14 18:04:24 +0100

    Graphic and functional changes

--- components/instant-chat/instant-chat-dock.tsx
+++ components/instant-chat/instant-chat-dock.tsx
@@ -1218,6 +1218,7 @@ export function InstantChatDock({
             strokeWidth="1.8"
             strokeLinecap="round"
             strokeLinejoin="round"
+            pointerEvents="none"
           >
             <path d="M21 15a4 4 0 0 1-4 4H8l-5 3V7a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4z" />
 
