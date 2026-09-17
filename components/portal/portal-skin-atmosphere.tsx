b16cdbe5b088e3c4b8c665429db5d7e42ca10fbb
Thiaric <thiaric@gmail.com>
2026-09-17 23:26:49 +0100

    Update portal-skin-atmosphere.tsx

--- components/portal/portal-skin-atmosphere.tsx
+++ components/portal/portal-skin-atmosphere.tsx
@@ -503,7 +503,7 @@ export function PortalSkinAtmosphere({
         .portal-atmosphere-engine {
   position: fixed;
   inset: 0;
-  z-index: 60;
+  z-index: 32;
   overflow: hidden;
   pointer-events: none;
 }
